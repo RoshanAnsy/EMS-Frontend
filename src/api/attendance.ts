e303bf6ff8969509789f073a9f3c5b71da644acb
Shivam Kumar Gaurav <shivamshroff111@gmail.com>
2025-08-29 07:22:14 +0530

    attendance summary added

---
+++ src/api/attendance.ts
@@ -0,0 +1,55 @@
+import axios from "axios";
+
+// ek ek attendance record
+export interface AttendanceRecord {
+  punchInTime: string;
+  punchOutTime: string;
+  PunchInLocation: string;
+  punchOutLocation: string;
+  status: string; // e.g., "PRESENT"
+}
+
+// data object -> keys dynamic dates hote hain (24/08/2025, 26/08/2025, etc.)
+export type AttendanceData = {
+  [date: string]: AttendanceRecord[];
+};
+
+// pagination ka type
+export interface AttendancePagination {
+  currentPage: number;
+  totalPages: number;
+  totalRecords: number;
+  hasNext: boolean;
+  hasPrev: boolean;
+  isApproximate: boolean;
+}
+
+// final API response
+export interface AttendanceApiResponse {
+  success: boolean;
+  message: string;
+  data: AttendanceData;
+  pagination: AttendancePagination;
+}
+
+
+export const getAttendanceSummary = async (
+  currentPage: number = 1,
+  pageLimit: number = 10,
+  startDate?: string,
+  endDate?: string
+): Promise<AttendanceApiResponse> => {
+  let url = `${process.env.NEXT_PUBLIC_API_URL}/attendance/attendance?page=${currentPage}&limit=${pageLimit}`;
+
+  if (startDate) url += `&startDate=${startDate}`;
+  if (endDate) url += `&endDate=${endDate}`;
+
+  const response = await axios.get<AttendanceApiResponse>(url, {
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem("token")}`,
+    },
+  });
+ console.log("this api res",response.data)
+  return response.data;
+};
+ 
